d0a8da4a71b94c9eb9cfc2bdf5fe890f7c053b37
udofia2 <enalsy22@gmail.com>
2020-09-26 02:44:00 +0100

    switch branch

---
+++ config/default.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://criminalRecord:bz49cRMd5VUq6OyV@criminal-registry-api-c.a3vdg.mongodb.net/criminalRecord?retryWrites=true&w=majority",
+  MONGO_DB: "mongodb://localhost:27017/criminal-registry-API",
+  mySecrete: "Brilliant Bright",
+  PORT: process.env.PORT || 3000,
+  url: "http://localhost",
+};

7aea7b873a4551afd71a527de2fb79bed0cb68b2
udofia2 <enalsy22@gmail.com>
2020-10-18 18:06:29 +0100

    request #16 solved

--- api/model/citizen.model.js
+++ api/model/citizen.model.js
@@ -3,52 +3,100 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const citizenSchema = new Schema({
-  name: {
-    first: {
-      type: String,
-      required: true,
-    },
-    last: {
-      type: String,
-      required: true,
-    },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName : {
+    type: String,
+    required: true
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  placeOfBirth: {
+    type: String,
+    required: true
+  },
+  residentialAddress: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: String,
+    required: true
+  },
+  nationality: {
+    type: String,
+    required: true
+  },
+  passportNum: {
+    type: String,
+    required: true
+  },
+  nationalIdentificationNumber: {
+    type: Number,
+    required: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
-  profileImage: String,
+  password1: {
+    type: String,
+    required: true
+  },
+  // name: {
+  //   first: {
+  //     type: String,
+  //     required: true,
+  //   },
+  //   last: {
+  //     type: String,
+  //     required: true,
+  //   },
+  // },
+  // email: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
+  // profileImage: String,
   // POB: {
   //   type: Date,
   //   required: true,
   // },
-  nationality: {
-    type: String,
-    required: true,
-  },
-  passport: {
-    number: {
-      type: Number,
-      require: true,
-    },
+  // nationality: {
+  //   type: String,
+  //   required: true,
+  // },
+  // passport: {
+  //   number: {
+  //     type: Number,
+  //     require: true,
+  //   },
 
-    date: {
-      type: Date,
-      required: true,
-    },
-    placeOfIssuance: {
-      type: String,
-      required: true,
-    },
-  },
-  fingerPrint: {
-    type: String,
-  },
+  //   date: {
+  //     type: Date,
+  //     required: true,
+  //   },
+  //   placeOfIssuance: {
+  //     type: String,
+  //     required: true,
+  //   },
+  // },
+  // fingerPrint: {
+  //   type: String,
+  // },
   // periodOfResidence: {
   //   type: Date,
   //   required: true,
